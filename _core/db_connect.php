3917d182867d19ffa2fb7e7e045624839953df60
nsanz <nsanz@192.168.1.149>
2026-02-26 09:52:53 -0600

    Making DB connect a little better

--- _core/db_connect.php
+++ _core/db_connect.php
@@ -5,4 +5,14 @@ $config = require __DIR__ . '/_core/config.php';
 
 require_once __DIR__ . '/_core/database.php';
 
-$pdo = db($config);
+function db(): PDO {
+	static $pdo = null;
+	if ($pdo) return $pdo;
+	
+	$dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', env('DB_HOST'), env('DB_NAME'));
+	$pdo = new PDO($dsn, env('DB_USER'), env('DB_PASS'), [
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	]);
+	return $pdo;
+}
